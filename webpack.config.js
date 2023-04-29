e90eb74a6c965e43ef86529a7ac61dea94d3589f
muradayev <muradali.main.github@gmail.com>
2023-04-29 17:54:15 +0400

    config file, json server, web-pack-server, node moodules

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["@babel/polyfill", './src/index.js'],
+  output: {
+    publicPath: "/",
+    path: path.resolve(__dirname, "bundles"),
+    filename: "bundled.js"
+  },
+  mode: "production",
+  devtool: "source-map",
+  devServer: {
+    port: 5500,
+    static: {
+      directory: path.join(__dirname, "/")
+    },
+    hot: true,
+    historyApiFallback: { index: "index.html" }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: { node: "12" } }]]
+          }
+        }
+      }
+    ]
+  }
+};
